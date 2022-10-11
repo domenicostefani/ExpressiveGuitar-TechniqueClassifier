9b528afcdd457b48be7a0a33fc14e0e5e96f71f3
domenicostefani <domenico.stefani@unitn.it>
2022-10-11 12:12:36 +0200

    fix file saving

--- run_multiple_training_sessions.py
+++ run_multiple_training_sessions.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python3
 
 import os
+import time
+import subprocess
+from glob import glob
+import shutil
+
+OUTPUT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'output')
+
+START_FROM_RUN_NUMBER = 1 # Change this to the run number you want to start from !!! WARNING !!! 1-based indexing
+NUM_PARALLEL_RUNS = 6 # Change this to the number of parallel runs you want to run
 
 
-print('Converting notebook to python script...')
-os.system('/home/user/Develop/Domenico/timbre-classifier/convert_to_script.py')
-print('Done.')
+start_from_run_index = START_FROM_RUN_NUMBER-1
+# print('Converting notebook to python script...')
+# os.system('/home/user/Develop/Domenico/timbre-classifier/convert_to_script.py')
+# print('Done.')
+
+
+runs_done = []
+runs_done_dict = {}
+
+infofiles = glob(os.path.join(OUTPUT_DIR,'*','info.txt'))
+for iff in infofiles:
+    if not os.path.exists(os.path.join(os.path.dirname(iff),'finalModel')) or\
+        not os.path.exists(os.path.join(os.path.dirname(iff),'backup_expressive-technique-classifier-phase3.py')):
+        print('Deleting '+os.path.dirname(iff))
+        shutil.rmtree(os.path.dirname(iff))
+    else:
+        commandline = ''
+        with open(iff) as oif:
+            oif.readline()
+            commandline = oif.readline()
+        assert 'expressive-technique-classifier-phase3.py -f ' in commandline
+        commandline = commandline.replace('expressive-technique-classifier-phase3.py -f ','').replace(' -d ',' ').replace(' -w ',' ').replace(' -dr ',' ').replace(' -lr ',' ').replace(' -bs ',' ').replace(' -e ',' ').replace(' -k ',' ')
+        runparams = commandline.strip().split(' ')
+        runs_done.append(runparams)
+        runs_done_dict[os.path.basename(os.path.dirname(iff))] = runparams
+
+print('Runs already in the output folder: '+str(len(runs_done)))
 
 print('#----------------------------#')
 print('# Parameter value ranges:    #')
@@ -44,13 +77,59 @@ product = list(itertools.product(*parameter_lists))
 assert len(product) == expected_length
 
 
-print('#----------------------------------#')
+
+
+# for strparameters in strproduct:
+#     if strparameters in runs_done:
+#         print('Run already done: '+str(strparameters) + '. Skipping...')
+
+
+strproduct = [[str(inel) for inel in el] for el in product]
+for rd in runs_done:
+    if not rd in strproduct:
+        curname = [k for k,v in runs_done_dict.items() if [str(e) for e in v] == rd][0]
+        raise Exception('Run "'+curname+'" with params '+str(rd)+' is in the output folder but not in the list of runs to do.')
+
+
+print('#--------------------------------------------------------------------#')
 print('# Number of combined runs:',len(product))
-print('#----------------------------------#')
+print('#')
+print('# Starting from run No.',start_from_run_index+1)
+print('# Which leaves '+str(len(product)-start_from_run_index)+' training runs to execute.')
+print('#--------------------------------------------------------------------#')
+
+product = product[start_from_run_index:]
 
 # Run the training sessions
+currently_running = []
+
 for i,parameters in enumerate(product):
-    print('Run',i+1,'of',len(product))
-    print('Parameters:',parameters)
-    features,net_depth,net_width,dropout_rate,learning_rate,batchsize,train_epochs,k_fold = parameters
-    print('python3 expressive-technique-classifier-phase3.py -f {} -d {} -w {} -dr {} -lr {} -bs {} -e {} -k {}'.format(features,net_depth,net_width,dropout_rate,learning_rate,batchsize,train_epochs,k_fold))
+
+    strparameters = [str(p) for p in parameters]
+    if strparameters in runs_done:
+        print('Run already done: '+str(strparameters) + '. Skipping...')
+    else:
+
+        # Busy wait until there is a free slot
+        while len(currently_running) >= NUM_PARALLEL_RUNS:
+            for process in currently_running:
+                if process.poll() is not None:
+                    currently_running.remove(process)
+            time.sleep(1)
+        
+        assert len(currently_running) < NUM_PARALLEL_RUNS
+
+        # Run the training session
+        print('Run',i+1+start_from_run_index,'of',len(product)+start_from_run_index)
+        print('Parameters:',parameters)
+        features,net_depth,net_width,dropout_rate,learning_rate,batchsize,train_epochs,k_fold = parameters
+        command = 'python3 expressive-technique-classifier-phase3.py -f {} -d {} -w {} -dr {} -lr {} -bs {} -e {} -k {}'.format(features,net_depth,net_width,dropout_rate,learning_rate,batchsize,train_epochs,k_fold)
+
+        print(command)
+
+        with open(os.devnull, 'w') as outfile:
+            process = subprocess.Popen(command.split(' '),stdout=outfile,stderr=outfile)
+            currently_running.append(process)
+        time.sleep(1)
+        
+    
